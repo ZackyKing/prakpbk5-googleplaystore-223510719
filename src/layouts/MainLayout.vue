e365bb4de9931eb7bdb75da1f40a44340ad36ddd
ZackyKing <zackyargya@gmail.com>
2024-05-27 22:43:33 +0700

    first comit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,72 @@
+<template>
+    <q-layout view="lHr lpR lfr">
+        <q-header class="bg-primary text-black" height-hint="98">
+            <q-toolbar>
+                <q-toolbar-title>
+                    <img class="custom-logo GPLAY__logo" src="https://cdn.quasar.dev/img/layout-gallery/logo-google-play.png">
+                </q-toolbar-title>
+                <q-tabs align="left" class="q-mr-md"> <!-- Add class q-mr-md to add margin to the right -->
+                    <q-route-tab @click="scrollToSection('games')" label="Games" />
+                    <q-route-tab @click="scrollToSection('apps')" label="Apps" />
+                    <q-route-tab @click="scrollToSection('movies')" label="Movies" />
+                    <q-route-tab @click="scrollToSection('books')" label="Books" />
+                    <q-route-tab @click="scrollToSection('children')" label="Children" />
+                </q-tabs>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+
+        <q-footer class="bg-primary text-black">
+            <q-toolbar>
+                <q-toolbar-title>
+                    Copyright © Google. All rights reserved.
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-footer>
+    </q-layout>
+</template>
+
+<style>
+.q-toolbar {
+    padding: 0px !important;
+}
+
+.q-toolbar__title:first-child {
+    padding-left: 32px !important;
+}
+
+.q-tab {
+    padding: 0 20px !important;
+    min-height: 75px !important;
+}
+
+.custom-logo {
+    position: relative;
+    top: 4px;
+    width: 180px; /* You can adjust the width as per your requirement */
+    height: auto; /* To maintain aspect ratio */
+}
+
+.q-tabs {
+    flex-grow: 4; /* To occupy remaining space */
+}
+
+/* Add this CSS to make the tab labels lowercase */
+.q-tab-label {
+    text-transform: lowercase;
+}
+</style>
+
+<script setup>
+    const scrollToSection = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            const yOffset = -50; // Adjust this value as needed
+            const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+    };
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,300 @@
+<template>
+    <q-page class="bg-primary">
+        <section class="bg-primary q-py-s games" id="games">
+                <div class="games-content q-pa-md">
+                    <q-carousel
+                    v-model="slide"
+                    infinite
+                    >
+                    <q-carousel-slide :name="1" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="2" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="3" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="4" img-src="../assets/Banner1.png" />
+                    </q-carousel>
+                </div>
+                <div class="games-content q-pa-md">
+                    <h5><b>Experience our top picks on PC</b></h5>
+                    <p>Download "Google Play Games" and get started</p>
+                </div>
+                <div class="card-container q-pa-md">
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                </div>
+        </section>
+        <section class="q-py-lg apps" id="apps">
+            <div class="apps-image-container apps-content relative-position">
+                <div class="games-content q-pa-md">
+                    <q-carousel
+                    v-model="slide"
+                    infinite
+                    >
+                    <q-carousel-slide :name="1" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="2" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="3" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="4" img-src="../assets/Banner1.png" />
+                    </q-carousel>
+                </div>
+                <div class="games-content q-pa-md">
+                    <h5><b>Experience our top picks on PC</b></h5>
+                    <p>Download "Google Play Games" and get started</p>
+                </div>
+                <div class="card-container q-pa-md">
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                </div>
+            </div>
+        </section>
+        <section class="q-py-lg bg-primary movies" id="movies">
+            <div class="movies-content">
+                <div class="games-content q-pa-md">
+                    <q-carousel
+                    v-model="slide"
+                    infinite
+                    >
+                    <q-carousel-slide :name="1" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="2" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="3" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="4" img-src="../assets/Banner1.png" />
+                    </q-carousel>
+                </div>
+                <div class="games-content q-pa-md">
+                    <h5><b>Experience our top picks on PC</b></h5>
+                    <p>Download "Google Play Games" and get started</p>
+                </div>
+                <div class="card-container q-pa-md">
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                </div>
+            </div>
+        </section>
+        <section class="q-py-lg books" id="books">
+            <div class="books-content">
+                <div class="games-content q-pa-md">
+                    <q-carousel
+                    v-model="slide"
+                    infinite
+                    >
+                    <q-carousel-slide :name="1" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="2" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="3" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="4" img-src="../assets/Banner1.png" />
+                    </q-carousel>
+                </div>
+                <div class="games-content q-pa-md">
+                    <h5><b>Experience our top picks on PC</b></h5>
+                    <p>Download "Google Play Games" and get started</p>
+                </div>
+                <div class="card-container q-pa-md">
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                </div>
+            </div>
+        </section>
+        <section class="q-py-lg bg-primary children" id="children">
+            <div class="children-content">
+                <div class="games-content q-pa-md">
+                    <q-carousel
+                    v-model="slide"
+                    infinite
+                    >
+                    <q-carousel-slide :name="1" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="2" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="3" img-src="../assets/Banner1.png" />
+                    <q-carousel-slide :name="4" img-src="../assets/Banner1.png" />
+                    </q-carousel>
+                </div>
+                <div class="games-content q-pa-md">
+                    <h5><b>Experience our top picks on PC</b></h5>
+                    <p>Download "Google Play Games" and get started</p>
+                </div>
+                <div class="card-container q-pa-md">
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                    <q-card class="my-card">
+                        <q-img src="../assets/arknightvideo.png">
+                        </q-img>
+                        <q-img src="../assets/arknight.png">
+                        </q-img>
+                    </q-card>
+                </div>
+            </div>
+        </section>
+    </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const slide = ref(1)
+const autoplay = ref(true)
+const email = ref('');
+const message = ref('');
+
+const submitFeedback = (e) => {
+    e.preventDefault();
+
+    console.log('Feedback submitted:', { email: email.value, message: message.value });
+};
+</script>
+
+<style>
+section {
+    min-height: 700px;
+    padding: 5rem 10% 2rem 9%;
+    font-family: 'Roboto';
+}
+
+/* games */
+.games-content {
+    border-radius: 100px;
+}
+
+.text-container {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    bottom: 10vh;
+    margin-right: 70px;
+}
+
+.text-container h2 {
+    line-height: 80px;
+}
+
+.text-container p {
+    margin-top: 20px;
+}
+
+.svg-container {
+    position: relative;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    width: 40%; 
+}
+
+.overlay-container {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.coffee-svg {
+    transform: rotate(180deg);
+    width: 400px; 
+    height: auto;
+    z-index: 1;
+    position: absolute;
+}
+
+.overlay-container img {
+    position: relative;
+    left: 5px;
+    bottom: 40px;
+    z-index: 2;
+}
+
+/* apps */
+.apps-us-content {
+    left: 1090px;
+    top: 981px;
+}
+
+.apps-us-content p:first-of-type {
+    margin-bottom: 8px;
+}
+
+.apps-us-content .bg-brown {
+    margin-bottom: 16px;
+}
+
+
+/* movies */
+.my-card {
+    width: calc(28.2% - 29px);
+    margin-right: 10px;
+    margin-bottom: 20px;
+}
+
+.card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+</style>
